efd3b6c56ff93d321e4c095a651d2fc681716c84
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-08-03 11:44:23 -0300

    feat(main): implement new div (aboutme) & scroll bar css

--- static/script.js
+++ static/script.js
@@ -6,7 +6,7 @@ const body = document.querySelector("body");
 menuMobile.addEventListener("click", () => {
     menuMobile.classList.contains("bi-list")
       ? menuMobile.classList.replace("bi-list", "bi-x")
-      : menuMobile.classList.replace("bi-x", "bi-list");
+      : menuMobile.classList.replace("bi-x", "bi-list")
       body.classList.toggle("menu-nav-active");
 });
 
